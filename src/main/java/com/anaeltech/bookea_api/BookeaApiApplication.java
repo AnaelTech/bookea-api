f1f0b60ac2581177f653bd10f8423927aa1045b9
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2026-04-08 13:15:54 +0200

    feat(init): ✨ Initialisation project

---
+++ src/main/java/com/anaeltech/bookea_api/BookeaApiApplication.java
@@ -0,0 +1,13 @@
+package com.anaeltech.bookea_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookeaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookeaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anaeltech/bookea_api/BookeaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anaeltech.bookea_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookeaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
