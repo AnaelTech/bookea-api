255a6d1acc0b5ec5589c937a8c4690f28a9f4292
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2026-04-10 12:34:00 +0200

    feat(service) : ✨ Add spring boot security for hash password into UserServiceImplement
    - Implements findAll methods and createUser
    - Use passwordEncoded to hash password of user

--- src/main/java/com/anaeltech/bookea_api/service/impl/UserServiceImplement.java
+++ src/main/java/com/anaeltech/bookea_api/service/impl/UserServiceImplement.java
@@ -2,31 +2,50 @@ package com.anaeltech.bookea_api.service.impl;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.anaeltech.bookea_api.dto.UserCreateDto;
 import com.anaeltech.bookea_api.dto.UserResponseDto;
 import com.anaeltech.bookea_api.dto.UserUpdateDto;
 import com.anaeltech.bookea_api.entity.User;
+import com.anaeltech.bookea_api.exceptions.EmailAlreadyExistException;
+import com.anaeltech.bookea_api.mapper.UserMapper;
+import com.anaeltech.bookea_api.repository.UserRepository;
 import com.anaeltech.bookea_api.service.UserService;
 
 @Service
 public class UserServiceImplement implements UserService {
 
+  private final UserRepository userRepository;
+  private final UserMapper userMapper;
+  private final PasswordEncoder passwordEncoder;
+
+  public UserServiceImplement(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
+    this.userRepository = userRepository;
+    this.userMapper = userMapper;
+    this.passwordEncoder = passwordEncoder;
+  }
+
   @Override
   public Page<UserResponseDto> findAll(Pageable pageable) {
+    return userRepository.findAll(pageable).map(userMapper::toDto);
   }
 
   @Override
   public UserResponseDto findById(Long id) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    return userRepository.findById(id).map(userMapper::toDto).orElse(null);
   }
 
   @Override
-  public UserResponseDto createUser(UserCreateDto user) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'createUser'");
+  public UserResponseDto createUser(UserCreateDto userCreateDto) {
+    if (userRepository.emailAlreadyExist(userCreateDto.getEmail())) {
+      throw new EmailAlreadyExistException(userCreateDto.getEmail());
+    }
+    String encodedPassword = passwordEncoder.encode(userCreateDto.getPassword());
+    userCreateDto.setPassword(encodedPassword);
+    User savedUser = userRepository.save(userMapper.toEntity(userCreateDto));
+    return userMapper.toDto(savedUser);
   }
 
   @Override
