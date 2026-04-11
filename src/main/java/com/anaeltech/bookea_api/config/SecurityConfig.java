11b1515f3c70bd9baea109a27b02445689978981
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2026-04-11 10:23:22 +0200

    feat(security): ✨ Add file SecurityConfig
    - Add PasswordEncoded to hash password
    - Disable Spring security

--- src/main/java/com/anaeltech/bookea_api/config/SecurityConfig.java
+++ src/main/java/com/anaeltech/bookea_api/config/SecurityConfig.java
@@ -2,11 +2,19 @@ package com.anaeltech.bookea_api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+    return http.build();
+  }
 
   @Bean
   public PasswordEncoder passwordEncoder() {
