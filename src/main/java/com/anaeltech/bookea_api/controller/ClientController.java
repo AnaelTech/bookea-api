17744b2f53d33876a25b42cbc561acb932c4be6e
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2026-04-20 12:28:55 +0200

    feat(controller): ✨ Implement CRUD endpoint for ClientController

--- src/main/java/com/anaeltech/bookea_api/controller/ClientController.java
+++ src/main/java/com/anaeltech/bookea_api/controller/ClientController.java
@@ -5,12 +5,19 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.anaeltech.bookea_api.dto.ClientCreateDto;
 import com.anaeltech.bookea_api.dto.ClientResponseDto;
+import com.anaeltech.bookea_api.dto.ClientUpdateDto;
+import com.anaeltech.bookea_api.exceptions.ClientNotFoundException;
 import com.anaeltech.bookea_api.exceptions.UserNotFoundException;
 import com.anaeltech.bookea_api.service.ClientService;
 import com.anaeltech.bookea_api.entity.Client;
@@ -18,8 +25,10 @@ import com.anaeltech.bookea_api.entity.Client;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.web.bind.annotation.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import io.swagger.v3.oas.annotations.Parameter;
 
 @RestController
@@ -57,5 +66,56 @@ public class ClientController {
 
     Page<ClientResponseDto> clients = clientService.findAll(pageable);
     return ResponseEntity.ok(clients);
+  }
+
+  @Operation(summary = "Get client by id", responses = {
+      @ApiResponse(responseCode = "200", description = "Successfully retrieved client", content = @Content(schema = @Schema(implementation = Client.class))),
+      @ApiResponse(responseCode = "400", description = "Invalid request", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "404", description = "Client not found", content = @Content(schema = @Schema(implementation = ClientNotFoundException.class))),
+      @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "503", description = "Service unavailable", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+  })
+  @GetMapping("/{id}")
+  public ResponseEntity<ClientResponseDto> getClientById(
+      @Parameter(description = "Client id", example = "1") @PathVariable("id") Long id) {
+    return ResponseEntity.ok(clientService.findById(id));
+  }
+
+  @Operation(summary = "Create client", responses = {
+      @ApiResponse(responseCode = "201", description = "Successfully created client", content = @Content(schema = @Schema(implementation = Client.class))),
+      @ApiResponse(responseCode = "400", description = "Invalid request", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+  })
+  @PostMapping
+  public ResponseEntity<ClientResponseDto> createClient(
+      @Parameter(description = "Client to create") @Valid @RequestBody ClientCreateDto client) {
+    ClientResponseDto clientResponseDto = clientService.createClient(client);
+    return ResponseEntity.status(201).body(clientResponseDto);
+  }
+
+  @Operation(summary = "Update client", responses = {
+      @ApiResponse(responseCode = "200", description = "Successfully updated client", content = @Content(schema = @Schema(implementation = Client.class))),
+      @ApiResponse(responseCode = "400", description = "Invalid request", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "404", description = "Client not found", content = @Content(schema = @Schema(implementation = ClientNotFoundException.class))),
+      @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+  })
+  @PutMapping("/{id}")
+  public ResponseEntity<ClientResponseDto> updateClient(
+      @Parameter(description = "Client to update") @PathVariable Long id,
+      @RequestBody ClientUpdateDto client) {
+    ClientResponseDto clientResponseDto = clientService.updateClient(id, client);
+    return ResponseEntity.ok(clientResponseDto);
+  }
+
+  @Operation(summary = "Delete client", responses = {
+      @ApiResponse(responseCode = "204", description = "Successfully deleted client", content = @Content(schema = @Schema(implementation = Client.class))),
+      @ApiResponse(responseCode = "400", description = "Invalid request", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "404", description = "Client not found", content = @Content(schema = @Schema(implementation = ClientNotFoundException.class))),
+      @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+  })
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteClient(@Parameter(description = "Client to delete") @PathVariable Long id) {
+    clientService.deleteClient(id);
+    return ResponseEntity.noContent().build();
   }
 }
